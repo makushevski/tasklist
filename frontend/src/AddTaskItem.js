05350ded31d379d69e66f23f62bd07c071188be7
mak <makushevski@gmail.com>
2024-02-10 18:55:54 +0500

    add react states and styles

--- frontend/src/AddTaskItem.js
+++ frontend/src/AddTaskItem.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function AddTaskItem(props){
-    return (<form>
-        
+    const[taskName, setTaskName] = useState('');
+    
+    const handleInputChange = (event) =>{
+        setTaskName(event.target.value);
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+        if(!taskName.trim()) return;
+        props.onAddTask(taskName);
+        setTaskName('');
+    }
+
+    return (<form onSubmit={handleSubmit}>
+        <label>Название задачи</label>
+        <input type='text' value={taskName} onChange={handleInputChange} />
+        <input type='submit' value='Добавить' />
     </form>)
 }
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import TaskList from './TaskList';
+import AddTaskItem from './AddTaskItem';
+import React, { useState } from 'react';
+
 
 function App() {
-  const tasks = [
-    { id: 1, name: "Выучить React"  },
-    { id: 2, name: "Выучить английский"  }
-  ]
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (taskName) =>{
+    var maxId = tasks.length > 0 ? Math.max(...tasks.map(x=>x.id)) + 1 : 1;
+
+    const newTask = {id: maxId, name: taskName}
+    setTasks([...tasks, newTask]);
+  }
 
   return (
     <div>
       <h1>Список задач</h1>
       <TaskList tasks={tasks} />
+      <AddTaskItem onAddTask={addTask} />
     </div>
   );
 }
